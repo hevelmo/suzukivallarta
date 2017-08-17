2932d8093e0c648e2d799a59f1f6eee31214de05
hevelmo <hevelmo060683@gmail.com>
2017-08-17 17:56:54 -0500

    commit

--- cache/19/19ed119d14f18be6dcea4b1bd024b5f010a255bb916a36afbc44c3b25eb3258a.php
+++ cache/19/19ed119d14f18be6dcea4b1bd024b5f010a255bb916a36afbc44c3b25eb3258a.php
@@ -22,6 +22,24 @@ class __TwigTemplate_e1d20494f9e174e854d477849928e233e3e7a31cca50c92b2c56b800c9d
             <div class=\"specification\" style=\"background-image: url('";
         // line 5
         echo twig_escape_filter($this->env, (isset($context["_host"]) ? $context["_host"] : null), "html", null, true);
+        echo "img/sections/home/slider/SLIDER-DE-CASINO-1659X655.jpg');\">
+                <div class=\"title-wrapper\" style=\"color: #fff\">
+                    <div class=\"title\">
+                       &nbsp;
+                    </div>
+                </div>
+                <div class=\"description extra-compact\">
+                    <p>
+                        <a class='button button-resp button-sm button-default' title='Casino Swift' target='_blank' onclick=\"ga('send', 'event', 'Casino Swift', 'Home Slider', 'Casino Swift');\" href=\"http://suzukigdl.com.mx/nochesdecasino/\">
+                            Registrate
+                        </a>
+                    </p>
+                </div>
+            </div>
+            <!-- slide extra -->
+            <div class=\"specification\" style=\"background-image: url('";
+        // line 20
+        echo twig_escape_filter($this->env, (isset($context["_host"]) ? $context["_host"] : null), "html", null, true);
         echo "img/sections/home/slider/01-SLIDER-DE-PROMOCION-SWIFT.jpg');\">
                 <div class=\"title-wrapper\" style=\"color: #fff\">
                     <div class=\"title\">
@@ -49,12 +67,12 @@ class __TwigTemplate_e1d20494f9e174e854d477849928e233e3e7a31cca50c92b2c56b800c9d
                 <div class=\"description compact\"><p>&nbsp;</p></div>
             </div>
             ";
-        // line 48
+        // line 63
         echo "            ";
-        // line 86
+        // line 101
         echo "            <!-- slide 00 -->
             <div class=\"specification\" style=\"background-image: url('";
-        // line 87
+        // line 102
         echo twig_escape_filter($this->env, (isset($context["_host"]) ? $context["_host"] : null), "html", null, true);
         echo "img/sections/home/slider/SLIDER-DE-PROMOCIONES-03.jpg');\">
                 <div class=\"title-wrapper\" style=\"color: #fff\">
@@ -65,10 +83,10 @@ class __TwigTemplate_e1d20494f9e174e854d477849928e233e3e7a31cca50c92b2c56b800c9d
                 <div class=\"description compact\"><p>&nbsp;</p></div>
             </div>
             ";
-        // line 112
+        // line 127
         echo "            <!-- slide 02 -->
             <div class=\"specification\" style=\"background-image: url('";
-        // line 113
+        // line 128
         echo twig_escape_filter($this->env, (isset($context["_host"]) ? $context["_host"] : null), "html", null, true);
         echo "img/sections/home/slider/slider_marca-2017.jpg');\">
                 <div class=\"title-wrapper\" style=\"color: #fff\">
@@ -79,7 +97,7 @@ class __TwigTemplate_e1d20494f9e174e854d477849928e233e3e7a31cca50c92b2c56b800c9d
                 <div class=\"description extra-compact\"><p>&nbsp;</p></div>
             </div>
             ";
-        // line 141
+        // line 156
         echo "        </div>
         <div class=\"clearfix\"></div>
     </div>
@@ -105,12 +123,27 @@ class __TwigTemplate_e1d20494f9e174e854d477849928e233e3e7a31cca50c92b2c56b800c9d
 
     public function getDebugInfo()
     {
-        return array (  83 => 141,  72 => 113,  69 => 112,  58 => 87,  55 => 86,  53 => 48,  24 => 5,  19 => 2,);
+        return array (  101 => 156,  90 => 128,  87 => 127,  76 => 102,  73 => 101,  71 => 63,  42 => 20,  24 => 5,  19 => 2,);
     }
 }
 /* {# INICIO DE NUEVO SLIDER #}*/
 /*     <div class="specifications-wrapper">*/
 /*         <div class="specifications" id="features-wrapper">*/
+/*             <!-- slide extra -->*/
+/*             <div class="specification" style="background-image: url('{{ _host }}img/sections/home/slider/SLIDER-DE-CASINO-1659X655.jpg');">*/
+/*                 <div class="title-wrapper" style="color: #fff">*/
+/*                     <div class="title">*/
+/*                        &nbsp;*/
+/*                     </div>*/
+/*                 </div>*/
+/*                 <div class="description extra-compact">*/
+/*                     <p>*/
+/*                         <a class='button button-resp button-sm button-default' title='Casino Swift' target='_blank' onclick="ga('send', 'event', 'Casino Swift', 'Home Slider', 'Casino Swift');" href="http://suzukigdl.com.mx/nochesdecasino/">*/
+/*                             Registrate*/
+/*                         </a>*/
+/*                     </p>*/
+/*                 </div>*/
+/*             </div>*/
 /*             <!-- slide extra -->*/
 /*             <div class="specification" style="background-image: url('{{ _host }}img/sections/home/slider/01-SLIDER-DE-PROMOCION-SWIFT.jpg');">*/
 /*                 <div class="title-wrapper" style="color: #fff">*/
